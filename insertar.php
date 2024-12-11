9cf33d7ccfc8c57cfaf73972237e22a325a9605c
unknown <alex.chinchi02@gmail.com>
2024-12-11 09:48:31 +0100

    hola

--- insertar.php
+++ insertar.php
@@ -1,23 +1,27 @@
 <?php
     require_once("inc/conexion.php");
 
-    $header = isset(getallheaders(['Authorization']));
-    if(isset($_POST["tituloCurso"], $_POST["textoCurso"], $_POST["imgCurso"])){
-        $tituloCurso = $_POST["tituloCurso"];
-        $textoCurso = $_POST["textoCurso"];
-        $imgCurso = $_POST["imgCurso"];
+    $header = isset(getallheaders(['Authorization']) ? getallheaders(['Authorization']) : null);
 
-        $consulta = "INSERT INTO cursosdb(tituloCurso, textoCurso, imgCurso) VALUES (?, ?, ?)";
-        $sentencia = $conexion->prepare($consulta);
-        $sentencia->execute([$tituloCurso, $textoCurso, $imgCurso]);
-        if($sentencia){
-            echo json_encode("OK");
+    if (substr($header,7) === '0123456789') {
+
+        if(isset($_POST["tituloCurso"], $_POST["textoCurso"], $_POST["imgCurso"])){
+            $tituloCurso = $_POST["tituloCurso"];
+            $textoCurso = $_POST["textoCurso"];
+            $imgCurso = $_POST["imgCurso"];
+
+            $consulta = "INSERT INTO cursosdb(tituloCurso, textoCurso, imgCurso) VALUES (?, ?, ?)";
+            $sentencia = $conexion->prepare($consulta);
+            $sentencia->execute([$tituloCurso, $textoCurso, $imgCurso]);
+            if($sentencia){
+                echo json_encode("OK");
+            }
+        }else{
+            header("Location:index.html");
         }
-    }else{
-        header("Location:index.html");
-    }
-
     
-
+    }else{
+        
+    }
 
 ?>
